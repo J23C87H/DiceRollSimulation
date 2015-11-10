e40659c28b311dad23c7dc467daddca44ea3711f
J23C87H <J23C87H@gmail.com>
2015-11-10 14:56:35 -0700

    Simple Dice Roll Simulation
    A simple dice roll simulation application using C#

---
+++ DiceSimulation1/DiceSimulation1/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace DiceSimulation1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl_die1 = new System.Windows.Forms.Label();
+            this.imgDice1 = new System.Windows.Forms.ImageList(this.components);
+            this.lbl_die2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Roll Dice";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_die1
+            // 
+            this.lbl_die1.ImageList = this.imgDice1;
+            this.lbl_die1.Location = new System.Drawing.Point(132, 46);
+            this.lbl_die1.Name = "lbl_die1";
+            this.lbl_die1.Size = new System.Drawing.Size(50, 50);
+            this.lbl_die1.TabIndex = 1;
+            // 
+            // imgDice1
+            // 
+            this.imgDice1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgDice1.ImageStream")));
+            this.imgDice1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgDice1.Images.SetKeyName(0, "Die1.png");
+            this.imgDice1.Images.SetKeyName(1, "Die2.png");
+            this.imgDice1.Images.SetKeyName(2, "Die3.png");
+            this.imgDice1.Images.SetKeyName(3, "Die4.png");
+            this.imgDice1.Images.SetKeyName(4, "Die5.png");
+            this.imgDice1.Images.SetKeyName(5, "Die6.png");
+            // 
+            // lbl_die2
+            // 
+            this.lbl_die2.ImageList = this.imgDice1;
+            this.lbl_die2.Location = new System.Drawing.Point(263, 46);
+            this.lbl_die2.Name = "lbl_die2";
+            this.lbl_die2.Size = new System.Drawing.Size(50, 50);
+            this.lbl_die2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(131, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dice 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(262, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Dice 2";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(12, 118);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(462, 323);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Roll #";
+            this.columnHeader1.Width = 56;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Die 1";
+            this.columnHeader2.Width = 56;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Die 2";
+            this.columnHeader3.Width = 57;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Result";
+            this.columnHeader4.Width = 289;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(187, 456);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 50);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Clear Results";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(304, 456);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 50);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 518);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_die2);
+            this.Controls.Add(this.lbl_die1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Dice Simulation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_die1;
+        private System.Windows.Forms.Label lbl_die2;
+        private System.Windows.Forms.ImageList imgDice1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ DiceSimulation1/DiceSimulation1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiceSimulation1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Clear list for new roll
+            listView1.Items.Clear();
+
+            //Setup random
+            Random shooter = new Random();
+
+            //Setup counter for For loop
+            for (int i = 1; i <= 100; ++i)
+            {
+                String result;
+                int die1, die2, die1Value, die2Value;
+
+                //Roll both dice
+                die1 = shooter.Next(6);
+                die2 = shooter.Next(6);
+
+                //Increase both dice by 1 to get dice value
+                die1Value = die1 + 1;
+                die2Value = die2 + 1;
+
+                //Change image for each Die to represent the number rolled
+                lbl_die1.ImageIndex = die1;
+                lbl_die2.ImageIndex = die2;
+
+                //if statement to determine if both dice were the same number
+                if (die1Value == die2Value)
+                {
+                    result = "On roll number " + i.ToString() + " both dice rolled " + die1Value.ToString() + "'s";
+                    string[] row = { i.ToString(), die1Value.ToString(), die2Value.ToString(), result };
+                    var listViewItem = new ListViewItem(row);
+                    listView1.Items.Add(listViewItem);
+                    //force application to update
+                    Application.DoEvents();
+                }
+                else
+                {
+
+                    string[] row = { i.ToString(), die1Value.ToString(), die2Value.ToString() };
+                    var listViewItem = new ListViewItem(row);
+                    listView1.Items.Add(listViewItem);
+                    //force application to update
+                    Application.DoEvents();
+                }
+
+                //Force thread to sleep to create a pause between each for loop
+                System.Threading.Thread.Sleep(600);
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Clear list view without re-rolling
+            listView1.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //alternate close application
+            this.Close();
+        }
+    }
+}
